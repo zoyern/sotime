bedc6781bc6e32f9081c640ab16c9eae76c4f4bd
zoyern <zoyern@gmail.com>
2024-08-05 21:18:19 +0200

    updating

--- src/timers/timers.c
+++ src/timers/timers.c
@@ -43,11 +43,12 @@ void	timer_list_clear(t_soloop *loop)
 
 void	sotime_update_timer(t_soloop *loop, t_sotimer *timer, int passed, int deleyed)
 {
+	(void)deleyed;
 	if (!timer)
 		return ;
 	if (timer->start)
 	{
-		timer->start_millis = loop->millis + deleyed;
+		timer->start_millis = loop->millis;
 		timer->millis = 0;
 		timer->working = 1;
 		timer->start = 0;
